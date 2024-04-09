7786232203228a49e890544a93dca70eca828e0c
filipetkovski <filip.petkovski.work@gmail.com>
2024-04-09 13:43:19 +0200

    added: analysis view for the manager

--- src/main/java/system/pos/javafx/controller/SettingsController.java
+++ src/main/java/system/pos/javafx/controller/SettingsController.java
@@ -9,6 +9,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import system.pos.javafx.controller.settingsControllers.OrderHistoryController;
 import system.pos.javafx.stage.StageListener;
+import system.pos.spring.exception.SettingsViewException;
 import system.pos.spring.model.Employee;
 
 @Component
@@ -33,7 +34,7 @@ public class SettingsController {
     private AnchorPane showSetting;
 
     @FXML
-    public void initialize() {
+    public void initialize() throws SettingsViewException {
         backButton.setCancelButton(true);
         changeIncludeSource(fxmlPath);
     }
@@ -47,51 +48,56 @@ public class SettingsController {
         stageListener.changeScene( "/fxml/homePage.fxml");
     }
 
-    public void openNewUser() {
+    public void openNewUser() throws SettingsViewException {
         fxmlPath = "/fxml/newEmployee.fxml";
         changeIncludeSource(fxmlPath);
     }
 
-    public void viewOrderHistory() {
+    public void viewOrderHistory() throws SettingsViewException {
         orderHistoryController.initData(employee);
         fxmlPath = "/fxml/orderHistory.fxml";
         changeIncludeSource(fxmlPath);
     }
 
-    public void viewShiftLogs() {
+    public void viewShiftLogs() throws SettingsViewException {
         fxmlPath = "/fxml/shiftLogs.fxml";
         changeIncludeSource(fxmlPath);
     }
 
-    public void viewOrderLogs() {
+    public void viewOrderLogs() throws SettingsViewException {
         fxmlPath = "/fxml/orderLogs.fxml";
         changeIncludeSource(fxmlPath);
     }
 
-    public void openNewCategory() {
+    public void openNewCategory() throws SettingsViewException {
         fxmlPath = "/fxml/newCategory.fxml";
         changeIncludeSource(fxmlPath);
     }
 
-    public void openNewProduct() {
+    public void openNewProduct() throws SettingsViewException {
         fxmlPath = "/fxml/newProduct.fxml";
         changeIncludeSource(fxmlPath);
     }
 
-    public void openNewTable() {
+    public void openNewTable() throws SettingsViewException {
         fxmlPath = "/fxml/newTable.fxml";
         changeIncludeSource(fxmlPath);
     }
 
+    public void openAnalysis() throws SettingsViewException {
+        fxmlPath = "/fxml/analysis.fxml";
+        changeIncludeSource(fxmlPath);
+    }
+
     //Change hte inside view
-    public void changeIncludeSource(String newFXMLPath) {
+    public void changeIncludeSource(String newFXMLPath) throws SettingsViewException {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(newFXMLPath));
             loader.setControllerFactory(applicationContext::getBean);
             Parent newContent = loader.load();
             showSetting.getChildren().setAll(newContent);
         } catch (Exception e) {
-            System.out.println("Error");
+            throw new SettingsViewException("Can't find the view to open.");
         }
     }
 }
